0772a20fd4c6607871e4732bd904315faf0c25d8
Holden Nakamura <holdenthehitcher@gmail.com>
2020-05-23 11:12:47 -1000

    task 2 added registerStudent function

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -6,11 +6,24 @@ class Student {
     }
 };
 
-class Bootcamp extends Student {
+class Bootcamp {
     constructor (name, level, students = []){
         this.name = name;
         this.level = level;
         this.students = students;
+    }
+
+    registerStudent(student) {
+        const studentAlreadyRegistered = this.students.filter(s => s.email === student.email);
+
+        if (studentAlreadyRegistered) {
+            console.log(`The student with ${student.email} is already registered`);
+        }
+
+        this.students.push(student);
+        console.log(`Registered student with the email ${student.email} has successfully been registered in ${this.name}`);
+        
+        return this.students;
     }
 
 };
