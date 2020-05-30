bb43d9281846ce65647ce56eb54554afac6bea1e
Holden Nakamura <holdenthehitcher@gmail.com>
2020-05-30 11:01:33 -1000

    refactored renderCampsite into CampsiteIntoComponent

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 export default class CampsiteInfo extends Component {
-    
-    render() {
-        return this.props.campsite ? <div clasName="row" /> : <div />
-    }
+  renderCampsite({image, name, description}) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={image} alt={name} />
+          <CardBody>
+            <CardTitle>{name}</CardTitle>
+            <CardText>{description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  render() {
+      const {campsite} = this.props
+    return campsite ? <div className="row">{this.renderCampsite(campsite)}</div> : <div />;
+  }
 }
-
-
-
