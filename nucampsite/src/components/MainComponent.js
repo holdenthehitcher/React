097ea316c1547844d6471b7d4e7fde3e9c63c224
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-06 11:48:02 -1000

    added <Media> to RenderPartners in AboutComponent

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -50,7 +50,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
-          <Route path="/about" component={About} />
+          <Route exact path="/about" render={() => <About partners={this.state.partners} />} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
